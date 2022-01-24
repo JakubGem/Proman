c0b1cca4e58e2d0d4a153fb3999e84f770f61249
justyna-eevee <justyna.zaczyk@onet.pl>
2022-01-24 16:03:53 +0100

    create urls: login, logout, register

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, request, redirect
 from dotenv import load_dotenv
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from util import json_response
 import mimetypes
-import query
+import users_queries, board_queries, status_queries
 
 mimetypes.add_type('application/javascript', '.js')
 app = Flask(__name__)
 load_dotenv()
+app.secret_key = 'proman'
 
 
 @app.route("/")
@@ -44,6 +44,47 @@ def main():
     # Serving the favicon
     with app.app_context():
         app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon/favicon.ico'))
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        my_user = users_queries.get_one_user(name)
+        if my_user and check_password_hash(my_user['password'], password):
+            session['login'] = True
+            session['user'] = my_user['name']
+            return redirect('/')
+        else:
+            return render_template('login.html', message="Incorrect user name or password")
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    if session['login'] == True:
+        session.pop('login')
+        session.pop('user')
+    return render_template('login.html', message="You are logged out")
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if 'login' in session:
+        return redirect('/')
+    elif request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        if users_queries.get_one_user(name):
+            return render_template('register.html', message="Username already exists, please choose another one!")
+
+        user_data = {'name': name,
+                     'email': email,
+                     'password': generate_password_hash(request.form['password'])}
+        users_queries.create_new_user(user_data)
+        return render_template('login.html', message="Successful registration. Log in to continue.")
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
