1e8721e5a277d76a54ac41c7d1533a9436558751
justyna-eevee <justyna.zaczyk@onet.pl>
2022-01-26 12:00:23 +0100

    paste function get_column_new_for_board

--- query/columns_queries.py
+++ query/columns_queries.py
@@ -1,6 +1,17 @@
 import data_manager
 
 
+def get_column_new_for_board(board_id):
+    return data_manager.execute_select(
+        """
+        SELECT id FROM columns
+        WHERE columns.board_id = %(board_id)s
+        AND columns.title = 'new'
+        ;
+        """
+        , {"board_id": board_id}, fetchall=False)
+
+
 def get_columns(board_id):
     matching_columns = data_manager.execute_select(
         """
