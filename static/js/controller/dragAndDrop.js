0e0e332234da09e057ab22d2f4cdf4c998ae5cb5
JakubGem <jakegemb89@gmail.com>
2022-02-09 13:33:32 +0100

    refactor

--- static/js/controller/dragAndDrop.js
+++ static/js/controller/dragAndDrop.js
@@ -12,10 +12,7 @@ cards.forEach((card) => {
 });
 
 empties.forEach((empty) => {
-
     empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
     empty.addEventListener('drop', dragDrop)
 });
 }
@@ -25,67 +22,38 @@ let cardId1 = 0
 
 function dragStart(e) {
     drop()
-
-
     div.card1 = e.currentTarget
-    // console.log(div.card1)
     let cardIdi = div.card1.getAttribute('data-card-id')
     cardId1 = cardIdi
-    // console.log(cardIdi)
     dragable = this;
-    console.log('start')
-    // return cardId1
 }
 
 function dragEnd() {
     dragable = null;
-    console.log('end')
 }
 
 function dragOver(e) {
     e.preventDefault();
-    console.log('dragover')
-}
-
-function dragEnter() {
-    console.log('enter')
-}
-
-function dragLeave() {
-    console.log('leave')
 }
 
 async function dragDrop(e) {
-
     let cardId = e.currentTarget
     let columnId = cardId.parentElement.getAttribute('data-column-id')
     cardId.insertAdjacentElement("afterbegin", div.card1)
     let addDiv = document.createElement('div')
     addDiv.className = 'empty'
     cardId.insertAdjacentElement('afterend', addDiv)
-    console.log('drop')
     await dataHandler.changeColumn(cardId1, columnId)
     await orderLIst()
 }
 
 async function orderLIst() {
-    let i = 0
     let a = 0
-    let lista = []
     let divs = document.getElementsByClassName('card')
-
     let cardsList = []
     for (a; a < divs.length; a++) {
         cardsList.push(divs[a].getAttribute('data-card-id'))
         console.log(cardsList)
-
     }
-    let cardId = document.getElementsByClassName('board-column')
-    for (i; i < cardId.length; i++) {
-        lista.push(cardId[i].getAttribute('data-column-id'))
-    }
-
     await dataHandler.cardOrderList(cardsList)
-
-
 }
